a0ac4d54bbe7a638c34975b2ea01ca2d507375a8
amabd7808 <amabd7808@7L0N6Y1.Student.UGDSB.ED>
2020-03-03 13:58:42 -0500

    prime

--- Eratosthenes/src/eratosthenes/Eratosthenes.java
+++ Eratosthenes/src/eratosthenes/Eratosthenes.java
@@ -6,7 +6,7 @@
 
 package eratosthenes;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Scanner;       //importing the scanner
 /**
  *
  * @author amabd7808
@@ -17,18 +17,27 @@ public class Eratosthenes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    Scanner input = new Scanner (System.in);
-    int n;
-    double [] number = new double [1001]; 
-        System.out.println("enter the number you want it stop at");
-        n = input.nextInt();
-        for(int i = 0; i <= n ; i++) {
-         if (n % i == 0) {
-            System.out.println(i);
-         }
+    Scanner input = new Scanner(System.in);
+    ArrayList<String> list=new ArrayList<String>(); //Creating arraylist    
+        System.out.println(" Please enter a number ");
+        int variable;       // introducing the number needed
+        variable = input.nextInt();
+        int number;
+        for ( int i=2 ; i <= variable ; i++) // Using a for loop to introduce i
+        { 
+             number = 0;                  //setting number to 0
+            for (int j = 2; j <= i / 2; j++)  // checking if it is prime
+            {
+                if (i % j ==0 ) 
+                {
+                    number++;
+                    break;
+                }
+            }
+            if (number == 0 && i != 1) // the prime number are getting outputted
+            {
+                System.out.println(i + "");
+            }
         }
     }
 }
-    
-    
-
